1b5c4571eff3ab28a0fcb67fe5778021faa7bcea
Evanildo Barros <evanildobarros@gmail.com>
2019-02-25 01:25:23 -0300

    versao 1.0

---
+++ index/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Gestor
+
+admin.site.register(Gestor)
+
+from .models import Funcionario
+
+admin.site.register(Funcionario)
+
+from .models import Clinica
+
+admin.site.register(Clinica)
+
+from .models import Cliente
+
+admin.site.register(Cliente)
---
+++ index/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+# Create your models here.
+
+class Gestor(models.Model):
+    gestor_id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    email = models.CharField(max_length=20, blank=True, null=True)
+    celular = models.CharField(max_length=11, blank=True, null=True)
+    especialidade = models.CharField(max_length=30, blank=True, null=True)
+
+class Funcionario(models.Model):
+    funcionario_id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    email = models.CharField(max_length=20, blank=True, null=True)
+    celular = models.CharField(max_length=11, blank=True, null=True)
+    especialidade = models.CharField(max_length=30, blank=True, null=True)
+
+
+class Clinica(models.Model):
+    clinica_id = models.AutoField(primary_key=True)
+    nomefantasia = models.CharField(max_length=100)
+    cnpj = models.CharField(unique=True, max_length=100)
+    email = models.CharField(max_length=20, blank=True, null=True)
+    contato = models.CharField(max_length=11, blank=True, null=True)
+    especialidade = models.CharField(max_length=30, blank=True, null=True)
+
+class Cliente(models.Model):
+    cliente_id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    email = models.CharField(max_length=20, blank=True, null=True)
+    celular = models.CharField(max_length=11, blank=True, null=True)
+    especialidade = models.CharField(max_length=30, blank=True, null=True)
+
