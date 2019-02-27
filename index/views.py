10dbcf484293a425d6a2090058245e149c2196d8
Evanildo Barros <evanildobarros@gmail.com>
2019-02-26 21:27:35 -0300

    versao 1.1

--- index/views.py
+++ index/views.py
@@ -1,6 +1,4 @@
-# Create your views here.
-from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'index.html')
