e282a85ca9c8c1af29000b6a20a3ec3b58e5fe8f
evanildojesus <evanildobarros@gmail.com>
2019-04-26 20:07:40 -0300

    'versao 2.0'

--- index/urls.py
+++ index/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from . import views
+from index import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('gestao.html', views.gestao, name='gestao'),
+    path('top.html', views.top, name='top'),
+    path('planejamento.html', views.planejamento, name='planejamento'),
+    path('marketing.html', views.marketing, name='marketing'),
+    path('design.html', views.design, name='design'),
+    path('data.html', views.data, name='data'),
 ]
--- index/views.py
+++ index/views.py
@@ -3,3 +3,22 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def gestao(request):
+    return render(request, 'gestao.html')
+
+def top(request):
+    return render(request, 'top.html')
+
+def planejamento(request):
+    return render(request, 'planejamento.html')
+
+def marketing(request):
+    return render(request, 'marketing.html')
+
+def design(request):
+    return render(request, 'design.html')
+
+def data(request):
+    return render(request, 'data.html')
+    
