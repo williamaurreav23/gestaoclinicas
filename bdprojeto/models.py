16d308ee11753c19cf053b58f51008c717f2df9f
Evanildo de Jesus <ejesus@localhost.localdomain>
2019-08-16 19:11:48 -0300

    versao backup

--- bdprojeto/models.py
+++ bdprojeto/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 # Create your models here.
+
+""" class contato(models.Model):
+    email = models.EmailField(_(""), max_length=254)
+    descricao = models.TextField(_(""))
+    def __str__(self):
+        return 
+
+    def __unicode__(self):
+        return 
+ """
