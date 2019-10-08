7f6318a6ed83bb910038d724b9a66951b3029503
Evanildo de Jesus <evanildodejesus@localhost.localdomain>
2019-10-07 21:25:26 -0300

    versao 3.5

--- index/forms.py
+++ index/forms.py
@@ -1,7 +1,11 @@
+
 from django import forms
 
 
-class ComentarioForm(forms.Form):
-    nome = forms.CharField(label='Nome', max_length=100)
-    comentario = forms.CharField(widget=forms.Textarea)
-    url = forms.URLField(label='url', max_length=100)
+class ClienteForms(forms.Form):
+
+        nome = forms.CharField(widget=forms.TextInput(attrs={'class': 'field'}))
+        sobrenome = forms.CharField(widget=forms.TextInput(attrs={'class': 'field'}))
+        cnpj = forms.CharField(widget=forms.TextInput(attrs={'class': 'field'}))
+        celular = forms.CharField(widget=forms.TextInput(attrs={'class': 'field'}))
+        email = forms.CharField(widget=forms.TextInput(attrs={'class': 'field'}))
